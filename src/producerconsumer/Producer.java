4144fc04f3776eba2fcfcfbee6053a9792546865
Jonas <jtb110@hotmail.com>
2015-09-04 10:06:06 +0200

    Producer class should be done now

--- src/producerconsumer/Producer.java
+++ src/producerconsumer/Producer.java
@@ -5,10 +5,42 @@
  */
 package producerconsumer;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Jonas
  */
-public class Producer {
-    
+public abstract class Producer implements Runnable {
+
+    private BlockingQueue<Long> in;
+    private BlockingQueue<Long> out;
+
+    public Producer(BlockingQueue<Long> in, BlockingQueue<Long> out) {
+        this.in = in;
+        this.out = out;
+
+    }
+
+    public void Run() {
+        Long c;
+//when c is in the thread pool and NOT null
+        while ((c = in.poll()) != null) {
+            try {
+                out.put(fibo(c));
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private long fibo(long n) {
+        if ((n == 0) || (n == 1)) {
+            return n;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
 }
--- src/producerconsumer/ProducerConsumer.java
+++ src/producerconsumer/ProducerConsumer.java
@@ -1,5 +1,7 @@
 package producerconsumer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -12,9 +14,21 @@ public class ProducerConsumer {
     private static BlockingQueue<Long> BQin = new ArrayBlockingQueue<>(20);
     private static BlockingQueue<Long> BQout = new ArrayBlockingQueue<>(20);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        BQin.add((long) 4);
+        BQin.add((long) 5);
+        BQin.add((long) 8);
+        BQin.add((long) 12);
+        BQin.add((long) 21);
+        BQin.add((long) 22);
+        BQin.add((long) 34);
+        BQin.add((long) 35);
+        BQin.add((long) 36);
+        BQin.add((long) 37);
+        BQin.add((long) 42);
+        
+        List<Thread> thread = new ArrayList<>();
 
-      
     }
 
 }
